11aa365db16c9ad21395b68f4c3364508beade54
Dominik <dominikborowy@gmail.com>
2020-05-06 11:33:44 +0200

    DB 2020-04-29

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,33 @@
 from app import app
+from flask import render_template
+from flaskext.markdown import Markdown
+from app.forms import ProductForm
+app.config['SECRET_KEY'] = '98dahs8da9h8jdjddj'
+
+Markdown(app)
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/scraper')
-def scraper():
-    return 'Podaj kod produktu do pobrania opinii'
+@app.route('/extract')
+def extract():
+    if request.method == 'POST':
+        return 'Przesłano formularz'
+    form = ProductForm()
+    return render_template('extract.html', form=form)
+
+@app.route('/products')
+def products():
+    pass
+
+@app.route('/about')
+def about():
+    content = ''
+    with open('README.md', 'r', encoding='utf-8') as f:
+        content = f.read()
+    return render_template('about.html', text=content)
 
 @app.route('/analyzer/<product_id>')
 def analyzer():
