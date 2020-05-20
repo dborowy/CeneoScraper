30637ede9e1798af137c6da0ce97187385ef51c3
Dominik <dominikborowy@gmail.com>
2020-05-20 11:36:11 +0200

    DB 2020-05-13

--- app/models.py
+++ app/models.py
@@ -1,8 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
 class Product():
     def __init__(self, product_id=None,name=None, opinions=[]):
         self.product_id = product_id
         self.name = name
         self.opinions = opinions
+
+    def extract_product(self):
+        page_response = requests.get('https://www.ceneo.pl/'+request.form['product_code'])
+        page_tree = BeautifulSoup(page_response.text, 'html.parser')
+        self.name = page_tree.select('hq.product_name').pop().get_text().strip()
+        try:
+            opinions_count=int(page_tree.select("a.product-reviews-link > span").pop().get_text().strip())
+        except IndexError:
+            opinions_count = 0
+        if opinions_count > 0:
+            url_prefix = 'https://www.ceneo.pl/'
+            url_postfix = '#tab=reviews'
+            url = url_prefix+'/'+product_id+url_postfix
+            while url:
+                page_response = requests.get(url)
+                page_tree = BeautifulSoup(page_response.text, 'html.parser')
+                opinions = page_tree.select('li.js_product-review')
+
+                #opinion = opinions[0]
+                for opinion in opinions:
+                    features = {key:extract_feature(opinion, *args)
+                                for key, args in selectors.items()}
+                    features['opinion_id'] = int(opinion['data-entry-id'])
+                    features['purchased'] = True if features['purchased'] == 'Opinia potwierdzona zakupem'else False
+                    features['useful'] = int(features['useful'])
+                    features['useless'] = int(features['useless'])
+                    features['pros'] = remove_whitespace(features['pros'])
+                    features['cons'] = remove_whitespace(features['cons'])
+                    
+                    opinions_list.append(features)
+
 
 class Opinion:
     selectors = {
@@ -33,11 +67,15 @@ class Opinion:
         self.purchased = purchased
         self.purchase_date = purchase_date
         self.review_date = review_date
+         
     def __str__(self):
         return(f'opinion id: {self.opinion_id}\n author: {self.author}\n')
+    
+    def __repr__(self):
+        pass
 
-
-
+    def extract_opinion(self):
+        pass
 
 opinion = Opinion()
 print(opinion)
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flaskext.markdown import Markdown
 from app.forms import ProductForm
 app.config['SECRET_KEY'] = '98dahs8da9h8jdjddj'
+from app.models import Product, Opinion
+import requests
 
 Markdown(app)
 
@@ -15,16 +17,22 @@ def index():
 def extract():
     form = ProductForm()
     if form.validate_on_submit():
-        return 'Przesłano formularz'
+        page_response = requests.get('https://www.ceneo.pl/'+request.form['product_code'])
+        if page_response.status_code == 200:
+            product = Product(request.form['product_code'])
+            return "OK"
+        else:
+            form.product_code.errors.append("Dla podanego kodu nie ma produktu")
+            return render_template('extract.html', form=form)
     return render_template('extract.html', form=form)
 
 @app.route('/products')
 def products():
     pass
 
-@app.route('/product/<product_id')
+@app.route('/product/<product_id>')
 def product():
-    
+    pass
 
 @app.route('/about')
 def about():
